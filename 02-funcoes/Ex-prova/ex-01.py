9c2b0d836754ff3828b4725948be50424df34666
Mateus <mateus.n2006@gmail.com>
2024-03-21 16:38:56 -0300

    Teste

---
+++ 02-funcoes/Ex-prova/ex-01.py
@@ -0,0 +1,3 @@
+'''Ex01 - Jogo de Adivinhação: Peça ao usuário para adivinhar um número entre 1 e 100 que o programa escolheu aleatoriamente. Informe ao usuário se o palpite está alto ou baixo, até que ele acerte o número.
+
+'''
